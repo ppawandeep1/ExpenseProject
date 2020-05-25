a416bcc85ee0c8854410376b9ee13b772675fb50
Manjinder-Kaur <manjinderkaur028@gmail.com>
2020-05-25 09:17:49 +0530

    adding header and footer to the project

---
+++ resources/views/header.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Expense System</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400,100,100italic,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<!-- banner -->
+    <div class="banner">
+        <div class="container">
+            <!-- header -->
+            <div class="header">
+                <div class="header-left">
+                    <img src="{{ asset('assets/images/logo.png') }}" style="width:144px;margin-top: 7px;margin-bottom: 7px;margin-left: 39px;"/>
+                    
+                </div>
+                <div class="header-left1">
+                    <span class="menu"><img src="images/menu.png" alt=" "></span>
+                    <ul class="nav1">
+                        <li class="hvr-sweep-to-bottom active"><a href="home.php">Home<i class="glyphicon glyphicon-home" aria-hidden="true"></i></a></li>
+                        <li class="hvr-sweep-to-bottom"><a href="about.php">About us<i class="glyphicon glyphicon-hand-right" aria-hidden="true"></i></a></li>
+                        <li class="hvr-sweep-to-bottom"><a href="services.php">Services<i class="glyphicon glyphicon-star-empty" aria-hidden="true"></i></a></li>
+                        <li class="hvr-sweep-to-bottom"><a href="products.php">Products<i class="glyphicon glyphicon-picture" aria-hidden="true"></i></a></li>
+                        <li class="hvr-sweep-to-bottom"><a href="contact.php">Contact us<i class="glyphicon glyphicon-envelope" aria-hidden="true"></i></a></li>
+                        <li class="hvr-sweep-to-bottom"><a href="login.php">Login<i class="glyphicon glyphicon-envelope" aria-hidden="true"></i></a></li>
+                          </ul>
+
+            </div>
+        </div>
+    </div>
